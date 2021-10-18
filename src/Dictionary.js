261535f8720e7cf73215996345ef9056ade0466f
kaylashovlowsky <85142863+kaylashovlowsky@users.noreply.github.com>
2021-10-17 17:26:44 -0700

    Updated form functionality

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [search, setSearch] = useState(" ");
+
+  function define(event) {
+    event.preventDefault();
+    alert(`Searching for ${search}...`);
+  }
+
+  function handleDefinition(event) {
+    setSearch(event.target.value);
+  }
+
   return (
     <div>
       <h1>Digital Dictionary</h1>
-      <form>
-        <input type="search" placeholder="Search..." className="search-bar" />
+      <form onSubmit={define}>
+        <input
+          type="search"
+          placeholder="Search..."
+          className="search-bar"
+          onChange={handleDefinition}
+        />
         <button type="submit" className="btn btn-primary">
           Define
         </button>
