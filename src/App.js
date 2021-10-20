c3f85afa8ea5709cb5b9d3e57601d911d547cfac
kaylashovlowsky <85142863+kaylashovlowsky@users.noreply.github.com>
2021-10-19 20:56:27 -0700

    Added link to open-source code on github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <div className="container">
         <header className="App-header"></header>
         <Dictionary />
+        <p>
+          <a href="https://github.com/kaylashovlowsky/react-dictionary-app">
+            Open-source code
+          </a>{" "}
+          by Kayla Shovlowsky
+        </p>
       </div>
     </div>
   );
