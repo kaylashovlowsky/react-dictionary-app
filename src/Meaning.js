f851aed75103bc492a5f64e8a8752c0687bc0041
kaylashovlowsky <85142863+kaylashovlowsky@users.noreply.github.com>
2021-10-20 19:33:47 -0700

    Added synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <h4>{props.meaning.definitions[0].definition}</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <h4>{props.meaning.definitions[0].definition}</h4>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
